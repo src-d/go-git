8b27a1deab8c84e4cfe7910608fa85369c813be7
Alberto Cortés <alberto@sourced.tech>
2016-05-24 09:54:26 +0200

    Add dummy file client

--- clients/common.go
+++ clients/common.go
@@ -16,6 +16,7 @@ import (
 	"net/url"
 
 	"gopkg.in/src-d/go-git.v3/clients/common"
+	"gopkg.in/src-d/go-git.v3/clients/file"
 	"gopkg.in/src-d/go-git.v3/clients/http"
 	"gopkg.in/src-d/go-git.v3/clients/ssh"
 )
@@ -25,6 +26,7 @@ var DefaultProtocols = map[string]common.GitUploadPackService{
 	"http":  http.NewGitUploadPackService(),
 	"https": http.NewGitUploadPackService(),
 	"ssh":   ssh.NewGitUploadPackService(),
+	"file":  file.NewGitUploadPackService(),
 }
 
 // KnownProtocols holds the current set of known protocols. Initially
--- clients/common_test.go
+++ clients/common_test.go
@@ -49,7 +49,7 @@ func (s *SuiteCommon) TestNewGitUploadPackService(c *C) {
 		{"http://github.com/src-d/go-git", false, "*http.GitUploadPackService"},
 		{"https://github.com/src-d/go-git", false, "*http.GitUploadPackService"},
 		{"ssh://github.com/src-d/go-git", false, "*ssh.GitUploadPackService"},
-		{"file://" + s.dirRemotePath, false, "*dir.GitUploadPackService"},
+		{"file://" + s.dirRemotePath, false, "*file.GitUploadPackService"},
 	}
 
 	for i, t := range tests {
