8799836bd265f65934517f50a85ce095868fe5f6
Alberto Cortés <alberto@sourced.tech>
2016-05-30 18:50:37 +0200

    remove dead code

--- formats/packfile/common.go
+++ formats/packfile/common.go
@@ -41,23 +41,3 @@ func (t *trackingReader) ReadByte() (c byte, err error) {
 	t.position++
 	return p[0], nil
 }
-
-// checkClose is used with defer to close the given io.Closer and check its
-// returned error value. If Close returns an error and the given *error
-// is not nil, *error is set to the error returned by Close.
-//
-// checkClose is typically used with named return values like so:
-//
-//   func do(obj *Object) (err error) {
-//     w, err := obj.Writer()
-//     if err != nil {
-//       return nil
-//     }
-//     defer checkClose(w, &err)
-//     // work with w
-//   }
-func checkClose(c io.Closer, err *error) {
-	if cerr := c.Close(); cerr != nil && *err == nil {
-		*err = cerr
-	}
-}
