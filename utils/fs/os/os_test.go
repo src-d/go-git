d8cbc6ac9ace388d3e6089867b3b72811b5aa10e
Santiago M. Mola <santi@mola.io>
2016-10-24 13:08:50 +0200

    utils/fs/os: use stdos alias for standard os.

--- utils/fs/os/os_test.go
+++ utils/fs/os/os_test.go
@@ -2,11 +2,11 @@ package os_test
 
 import (
 	"io/ioutil"
-	"os"
+	stdos "os"
 	"testing"
 
 	. "gopkg.in/check.v1"
-	osfs "gopkg.in/src-d/go-git.v4/utils/fs/os"
+	"gopkg.in/src-d/go-git.v4/utils/fs/os"
 	"gopkg.in/src-d/go-git.v4/utils/fs/test"
 )
 
@@ -20,10 +20,10 @@ type OSSuite struct {
 var _ = Suite(&OSSuite{})
 
 func (s *OSSuite) SetUpTest(c *C) {
-	s.path, _ = ioutil.TempDir(os.TempDir(), "go-git-os-fs-test")
-	s.FilesystemSuite.Fs = osfs.NewOS(s.path)
+	s.path, _ = ioutil.TempDir(stdos.TempDir(), "go-git-os-fs-test")
+	s.FilesystemSuite.Fs = os.NewOS(s.path)
 }
 func (s *OSSuite) TearDownTest(c *C) {
-	err := os.RemoveAll(s.path)
+	err := stdos.RemoveAll(s.path)
 	c.Assert(err, IsNil)
 }
