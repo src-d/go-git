6186859c5c2321817fe5e5e146e8380434141c0c
Santiago M. Mola <santi@mola.io>
2016-10-24 12:56:28 +0200

    ensure utils/fs/os tests are run

--- utils/fs/os/os_test.go
+++ utils/fs/os/os_test.go
@@ -3,14 +3,17 @@ package os_test
 import (
 	"io/ioutil"
 	"os"
+	"testing"
 
 	. "gopkg.in/check.v1"
 	osfs "gopkg.in/src-d/go-git.v4/utils/fs/os"
-	. "gopkg.in/src-d/go-git.v4/utils/fs/test"
+	"gopkg.in/src-d/go-git.v4/utils/fs/test"
 )
 
+func Test(t *testing.T) { TestingT(t) }
+
 type OSSuite struct {
-	FilesystemSuite
+	test.FilesystemSuite
 	path string
 }
 
