7006cac2fe8c99c78172208172c22d50a376d808
Santiago M. Mola <santi@mola.io>
2016-11-08 13:20:16 +0100

    commit_walker: simplify implementation.
    * No recursive calls.
    * Check if seen before adding to the queue.

--- commit_walker.go
+++ commit_walker.go
@@ -6,62 +6,40 @@ import (
 	"gopkg.in/src-d/go-git.v4/core"
 )
 
-type commitWalker struct {
-	seen  map[core.Hash]bool
-	stack []*CommitIter
-	start *Commit
-	cb    func(*Commit) error
-}
-
 // WalkCommitHistory walks the commit history
 func WalkCommitHistory(c *Commit, cb func(*Commit) error) error {
-	w := &commitWalker{
-		seen:  make(map[core.Hash]bool),
-		stack: make([]*CommitIter, 0),
-		start: c,
-		cb:    cb,
-	}
+	seen := map[core.Hash]bool{c.Hash: true}
+	queue := []*Commit{c}
 
-	return w.walk()
-}
-
-func (w *commitWalker) walk() error {
-	var commit *Commit
-
-	if w.start != nil {
-		commit = w.start
-		w.start = nil
-	} else {
-		current := len(w.stack) - 1
-		if current < 0 {
+	for {
+		if len(queue) == 0 {
 			return nil
 		}
 
-		var err error
-		commit, err = w.stack[current].Next()
-		if err == io.EOF {
-			w.stack = w.stack[:current]
-			return w.walk()
-		}
+		commit := queue[0]
+		queue = queue[1:]
 
-		if err != nil {
+		if err := cb(commit); err != nil {
 			return err
 		}
-	}
 
-	// check and update seen
-	if w.seen[commit.Hash] {
-		return w.walk()
-	}
+		iter := commit.Parents()
+		for {
+			parent, err := iter.Next()
+			if err == io.EOF {
+				break
+			}
 
-	w.seen[commit.Hash] = true
-	if commit.NumParents() > 0 {
-		w.stack = append(w.stack, commit.Parents())
-	}
+			if err != nil {
+				return err
+			}
 
-	if err := w.cb(commit); err != nil {
-		return err
-	}
+			if seen[parent.Hash] {
+				continue
+			}
 
-	return w.walk()
+			seen[parent.Hash] = true
+			queue = append(queue, parent)
+		}
+	}
 }
