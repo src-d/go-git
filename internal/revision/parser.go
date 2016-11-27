f449b4c14d7e51d544e527eee818ffb92e65cb32
antham <hamonanth@gmail.com>
2016-11-27 23:01:18 +0100

    internal/revision : add reference name parser
    * reference name parse follows rules described here : https://git-scm.com/docs/git-check-ref-format

--- internal/revision/parser.go
+++ internal/revision/parser.go
@@ -1,8 +1,18 @@
 package revision
 
 import (
+	"fmt"
 	"io"
 )
+
+// ErrInvalidRevision is emitted if string doesn't match valid revision
+type ErrInvalidRevision struct {
+	s string
+}
+
+func (e *ErrInvalidRevision) Error() string {
+	return "Revision invalid : " + e.s
+}
 
 // parser represents a parser.
 type parser struct {
@@ -35,3 +45,68 @@ func (p *parser) scan() (tok token, lit string) {
 
 // unscan pushes the previously read token back onto the buffer.
 func (p *parser) unscan() { p.buf.n = 1 }
+
+// parseRef extract reference name
+func (p *parser) parseRef() (string, error) {
+	var tok token
+	var prevTok token
+	var lit string
+	var buf string
+
+	for {
+		tok, lit = p.scan()
+
+		err := p.checkRefFormat(tok, lit, prevTok, buf)
+
+		if err != nil {
+			return "", err
+		}
+
+		switch tok {
+		case eof:
+			return buf, nil
+		}
+
+		buf += lit
+		prevTok = tok
+	}
+}
+
+// checkRefFormat ensure reference name follow rules defined here :
+// https://git-scm.com/docs/git-check-ref-format
+func (p *parser) checkRefFormat(token token, literal string, previousToken token, buffer string) error {
+	switch token {
+	case aslash, space, control, qmark, asterisk, obracket:
+		return &ErrInvalidRevision{fmt.Sprintf(`must not contains "%s"`, literal)}
+	}
+
+	if (token == dot || token == slash) && buffer == "" {
+		return &ErrInvalidRevision{fmt.Sprintf(`must not start with "%s"`, literal)}
+	}
+
+	if previousToken == slash && token == eof {
+		return &ErrInvalidRevision{`must not end with "/"`}
+	}
+
+	if previousToken == dot && token == eof {
+		return &ErrInvalidRevision{`must not end with "."`}
+	}
+
+	if token == dot && previousToken == slash {
+		return &ErrInvalidRevision{`must not contains "/."`}
+	}
+
+	if previousToken == dot && token == dot {
+		return &ErrInvalidRevision{`must not contains ".."`}
+	}
+
+	if previousToken == slash && token == slash {
+		return &ErrInvalidRevision{`must not contains consecutively "/"`}
+	}
+
+	if (token == slash || token == eof) && len(buffer) > 4 && buffer[len(buffer)-5:] == ".lock" {
+		return &ErrInvalidRevision{"cannot end with .lock"}
+	}
+
+	return nil
+}
--- internal/revision/parser_test.go
+++ internal/revision/parser_test.go
@@ -68,3 +68,54 @@ func (s *ParserSuite) TestUnscan(c *C) {
 	c.Assert(str, Equals, "Hello")
 	c.Assert(tok, Equals, word)
 }
+
+func (s *ParserSuite) TestParseRefWithValidName(c *C) {
+	datas := []string{
+		"lock",
+		"master",
+		"v1.0.0",
+		"refs/stash",
+		"refs/tags/v1.0.0",
+		"refs/heads/master",
+		"refs/remotes/test",
+		"refs/remotes/origin/HEAD",
+		"refs/remotes/origin/master",
+	}
+
+	for _, d := range datas {
+		parser := newParser(bytes.NewBufferString(d))
+
+		result, err := parser.parseRef()
+
+		c.Assert(err, Equals, nil)
+		c.Assert(result, Equals, d)
+	}
+}
+
+func (s *ParserSuite) TestParseRefWithUnvalidName(c *C) {
+	datas := map[string]error{
+		".master":                     &ErrInvalidRevision{`must not start with "."`},
+		"/master":                     &ErrInvalidRevision{`must not start with "/"`},
+		"master/":                     &ErrInvalidRevision{`must not end with "/"`},
+		"master.":                     &ErrInvalidRevision{`must not end with "."`},
+		"refs/remotes/.origin/HEAD":   &ErrInvalidRevision{`must not contains "/."`},
+		"test..test":                  &ErrInvalidRevision{`must not contains ".."`},
+		"test..":                      &ErrInvalidRevision{`must not contains ".."`},
+		"test test":                   &ErrInvalidRevision{`must not contains " "`},
+		"test*test":                   &ErrInvalidRevision{`must not contains "*"`},
+		"test?test":                   &ErrInvalidRevision{`must not contains "?"`},
+		"test\\test":                  &ErrInvalidRevision{`must not contains "\"`},
+		"test[test":                   &ErrInvalidRevision{`must not contains "["`},
+		"te//st":                      &ErrInvalidRevision{`must not contains consecutively "/"`},
+		"refs/remotes/test.lock/HEAD": &ErrInvalidRevision{`cannot end with .lock`},
+		"test.lock":                   &ErrInvalidRevision{`cannot end with .lock`},
+	}
+
+	for s, e := range datas {
+		parser := newParser(bytes.NewBufferString(s))
+
+		_, err := parser.parseRef()
+
+		c.Assert(err, DeepEquals, e)
+	}
+}
