6fd926574eab5e8bf14c4a56764a0268818aba73
Máximo Cuadros <mcuadros@gmail.com>
2016-11-30 15:41:27 +0100

    protocol/pakp: UploadReq validation and creation of capabilities

--- plumbing/protocol/packp/upload_pack_request.go
+++ plumbing/protocol/packp/upload_pack_request.go
@@ -6,7 +6,57 @@ import (
 
 	"gopkg.in/src-d/go-git.v4/plumbing"
 	"gopkg.in/src-d/go-git.v4/plumbing/format/pktline"
+	"gopkg.in/src-d/go-git.v4/plumbing/protocol/packp/capability"
 )
+
+// UploadPackRequest represents a upload-pack request.
+// Zero-value is not safe, use NewUploadPackRequest instead.
+type UploadPackRequest struct {
+	*UploadRequest
+	*UploadHaves
+}
+
+// NewUploadPackRequest creates a new UploadPackRequest and returns a pointer.
+func NewUploadPackRequest() *UploadPackRequest {
+	return &UploadPackRequest{
+		UploadHaves:   &UploadHaves{},
+		UploadRequest: NewUploadRequest(),
+	}
+}
+
+// NewUploadPackRequestFromCapabilities creates a new UploadPackRequest and
+// returns a pointer. The request capabilities are filled with the most optiomal
+// ones, based on the adv value (advertaised capabilities), the UploadPackRequest
+// it has no wants, haves or shallows and an infinite depth
+func NewUploadPackRequestFromCapabilities(adv *capability.List) *UploadPackRequest {
+	return &UploadPackRequest{
+		UploadHaves:   &UploadHaves{},
+		UploadRequest: NewUploadRequestFromCapabilities(adv),
+	}
+}
+
+// IsEmpty returns true if the request wants is equal to zero
+func (r *UploadPackRequest) IsEmpty() bool {
+	return isSubset(r.Wants, r.Haves)
+}
+
+func isSubset(needle []plumbing.Hash, haystack []plumbing.Hash) bool {
+	for _, h := range needle {
+		found := false
+		for _, oh := range haystack {
+			if h == oh {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return false
+		}
+	}
+
+	return true
+}
 
 // UploadHaves is a message to signal the references that a client has in a
 // upload-pack. Do not use this directly. Use UploadPackRequest request instead.
@@ -30,46 +80,4 @@ func (u *UploadHaves) Encode(w io.Writer) error {
 	}
 
 	return nil
-}
-
-// Have adds a hash reference to the 'haves' list.
-func (r *UploadHaves) Have(h ...plumbing.Hash) {
-	r.Haves = append(r.Haves, h...)
-}
-
-// UploadPackRequest represents a upload-pack request.
-// Zero-value is not safe, use NewUploadPackRequest instead.
-type UploadPackRequest struct {
-	*UploadRequest
-	*UploadHaves
-}
-
-// NewUploadPackRequest creates a new UploadPackRequest and returns a pointer.
-func NewUploadPackRequest() *UploadPackRequest {
-	return &UploadPackRequest{
-		UploadHaves:   &UploadHaves{},
-		UploadRequest: NewUploadRequest(),
-	}
-}
-
-func (r *UploadPackRequest) IsEmpty() bool {
-	return isSubset(r.Wants, r.Haves)
-}
-
-func isSubset(needle []plumbing.Hash, haystack []plumbing.Hash) bool {
-	for _, h := range needle {
-		found := false
-		for _, oh := range haystack {
-			if h == oh {
-				found = true
-				break
-			}
-		}
-
-		if !found {
-			return false
-		}
-	}
-
-	return true
 }
