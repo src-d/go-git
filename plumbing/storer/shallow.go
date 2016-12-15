9f308b4f8550a6e4fa8c812d4e401322afa78085
Máximo Cuadros <mcuadros@gmail.com>
2016-12-15 13:45:28 +0100

    changes

--- plumbing/storer/shallow.go
+++ plumbing/storer/shallow.go
@@ -2,8 +2,8 @@ package storer
 
 import "gopkg.in/src-d/go-git.v4/plumbing"
 
-// ShallowStorer storage of shallow commits, meaning that it does not have the
-// parents of a commit (explanation from git documentation)
+// ShallowStorer storage of references to shallow commits by hash, meaning that
+// these commits have missing parents because of a shallow fetch.
 type ShallowStorer interface {
 	SetShallow([]plumbing.Hash) error
 	Shallow() ([]plumbing.Hash, error)
